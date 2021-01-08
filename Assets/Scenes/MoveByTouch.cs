b5e5c033bdafe0d833a34e6b675c9ea37c9c0cf5
Haseeb <haseeb.ahmad@ozitechnology.com>
2021-01-09 01:53:10 +0500

    Arqam's work

---
+++ Assets/Scenes/MoveByTouch.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveByTouch : MonoBehaviour
+{
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+    public float zDistance = 30.0f;
+    [HideInInspector]
+    public bool isThrown;
+    bool ControlActivate = false;
+    public GameObject[] Targets;
+    // public Transform targetPosition;
+    int targetindex;
+
+    void Start()
+    {
+        isThrown = false;
+    }
+
+    void Update()
+    {
+        if (!ControlActivate) return;
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+ 
+        if (isThrown)
+        {
+            transform.Rotate(0f, 0f, 10f);
+         //   return;
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 mousePos = Input.mousePosition * -1.0f;
+            mousePos.z = zDistance; 
+
+            startPos = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            Vector3 mousePos = Input.mousePosition * -1.0f;
+            mousePos.z = zDistance;
+
+
+            endPos = Camera.main.ScreenToWorldPoint(mousePos);
+            endPos.z = Camera.main.nearClipPlane; 
+
+            Vector3 throwDir = (startPos - endPos).normalized;
+            float angle = Vector2.Angle(Vector3.right, endPos - startPos);
+            Debug.LogError(angle);
+            if(angle>20&&angle<70)
+            {
+                targetindex = 0;
+            }
+            else if(angle>70 && angle<110)
+            {
+                targetindex = 1;
+            }
+            else if(angle>110 &&angle<130)
+            {
+                targetindex = 2;
+            }
+            //this.gameObject.GetComponent<Rigidbody>().AddForce(throwDir * (startPos - endPos).sqrMagnitude);
+            //this.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            isThrown = true;
+          LeanTween.move(gameObject, Targets[targetindex].transform.position, 1f).setOnComplete(stopRotation);
+        }
+    }
+
+    private void stopRotation()
+    {
+        isThrown = false;
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void OnMouseDown()
+    {
+        ControlActivate = true;
+    }
+
+
+}
---
+++ Assets/Scenes/SenseTarget.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenseTarget : MonoBehaviour
+{
+
+    public float radius = 1f;
+    public bool isDrawGizmoz;
+    public float speed = 1.0f;
+    private GameObject book;
+    private bool setBookPosition = false;
+    public Transform target;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //RaycastHit hit;
+        //if (Physics.SphereCast(transform.position, radius, transform.forward, out hit, 10))
+        //{
+        //    if (hit.collider.gameObject.GetComponent<MoveByTouch>())
+        //    {
+        //        book = hit.collider.gameObject;
+        //        book.GetComponent<Rigidbody>().useGravity = false;
+        //        book.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        //        setBookPosition = true;
+        //    }
+        //    Debug.Log(hit.collider.gameObject.name);
+        //}
+
+        if (setBookPosition)
+        {
+            float step = speed * Time.deltaTime;
+            book.transform.position = Vector3.MoveTowards(book.transform.position, target.position, step);
+            if (book.transform.position == target.position)
+            {
+                book.GetComponent<MoveByTouch>().isThrown = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        book = other.gameObject;
+        book.GetComponent<Rigidbody>().useGravity = false;
+        book.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        setBookPosition = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (isDrawGizmoz)
+        {
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+    }
+}
