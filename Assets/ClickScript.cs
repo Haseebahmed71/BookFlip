942841b934ce100ca094ff588c8a9e93975c220d
Haseeb <haseeb.ahmad@ozitechnology.com>
2021-01-09 14:21:32 +0500

    UI Adjusted

---
+++ Assets/ClickScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ClickScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(PlaySound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void PlaySound()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Assets/UIHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AutoFlip _book;
+    public float FlipDelay;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void PlayBtnHandler()
+    {
+        Invoke("FlipPage", FlipDelay);
+    }
+    void FlipPage()
+    {
+        _book.FlipRightPage();
+    }
+}
